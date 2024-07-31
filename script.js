a6c9e710b705b6dae50607f947186643fd8e2e99
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-31 16:26:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // write js code here if required
+document.addEventListener('DOMContentLoaded',()=>{
+	const timer = document.querySelector('#timer');
+
+	const d = new Date();
+	const msg = `${d.getDate()}/${d.getMonth()+1}/${getFullYear}, ${getHours}:${getMinutes}:${getSeconds}`;
+	timer.innerHTML = msg;
+});
