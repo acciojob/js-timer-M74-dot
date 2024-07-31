12cf9cf8082335113133f0b1557a0d267a70113b
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-31 16:30:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded',()=>{
 	const timer = document.querySelector('#timer');
 
 	const d = new Date();
-	const msg = `${d.getDate()}/${d.getMonth()+1}/${getFullYear}, ${getHours}:${getMinutes}:${getSeconds}`;
+	const msg = `${d.getDate()}/${d.getMonth()+1}/${getFullYear()}, ${getHours()}:${getMinutes()}:${getSeconds()}`;
 	timer.innerHTML = msg;
 });
